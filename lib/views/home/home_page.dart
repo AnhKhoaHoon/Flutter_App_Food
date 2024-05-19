191a4edeed8ec3ce7597d335179fc9d38b316645
John Doe <johndoe@example.com>
2024-05-19 21:08:18 +0700

    CategoryList

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:demo/common/custom_appbar.dart';
 import 'package:demo/common/custom_container.dart';
 import 'package:demo/constants/constants.dart';
+import 'package:demo/views/home/widgets/category_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -11,11 +12,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimary,
-      appBar: PreferredSize(preferredSize: Size.fromHeight(130.h),
-      child:CustomAppBar()),
-      body:SafeArea(
-        child: CustomContainer(containerContent: Container()),
-        ),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(130.h), child: const CustomAppBar()),
+      body: SafeArea(
+        child: CustomContainer(
+            containerContent: Column(
+          children: [CategoryList()],
+        )),
+      ),
     );
   }
 }
