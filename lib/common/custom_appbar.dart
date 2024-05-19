4bfecbb5b20e68a1b4c3e302a70271251360f247
John Doe <johndoe@example.com>
2024-05-19 20:13:54 +0700

    Custom AppBar Time Function

--- lib/common/custom_appbar.dart
+++ lib/common/custom_appbar.dart
@@ -26,6 +26,8 @@ class CustomAppBar extends StatelessWidget {
                     CircleAvatar(
                       radius: 22.r,
                       backgroundColor: kSecondary,
+                      backgroundImage: const NetworkImage(
+                          'https://scontent.fsgn5-13.fna.fbcdn.net/v/t39.30808-6/426565295_718904967073323_7873619382949730128_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=QhN35ksUSU8Q7kNvgEoF5YK&_nc_ht=scontent.fsgn5-13.fna&oh=00_AYB5eg3bq4Jc_j0te82lIwpOBN3t93tWVfnGUij6EoR_hw&oe=664FD2FD'),
                     ),
                     Padding(
                       padding: EdgeInsets.only(bottom: 6.h, left: 8.w),
@@ -54,5 +56,15 @@ class CustomAppBar extends StatelessWidget {
                 ),
               ],
             )));
+  }
+
+  String getTimeOfDay() {
+    DateTime now = DateTime.now();
+    int hour = now.hour;
+    if (hour >= 0 && hour < 12) {
+      return '';
+    } else if (hour >= 12 && hour < 16) {
+      return '';
+    } else {}
   }
 }
